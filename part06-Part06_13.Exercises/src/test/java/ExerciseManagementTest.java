7f08503c2b6a641f87ceaa85faf2e76a5f22919c
Teh Chen Ming <101413042+chenming7777@users.noreply.github.com>
2023-05-13 11:35:22 +0800

    Add files via upload

--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,16 +1,14 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-import org.junit.Before;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import org.junit.Test;
+
+
+
 
 public class ExerciseManagementTest {
     private ExerciseManagement management;
-    
-    @Before
-    public void initialize(){
-        management = new ExerciseManagement();
-    }
     
     @Test
     public void exerciseListEmptyAtBeginning(){
@@ -29,16 +27,16 @@ public class ExerciseManagementTest {
         assertTrue(management.exerciseList().contains("Write a test"));
     }
     
-    @Test
     public void exerciseCanBeMarkedAsCompleted(){
         management.add("New exercise");
         management.markAsCompleted("New exercise");
         assertTrue(management.isCompleted("New exercise"));
     }
     
-    @Test public void ifNotMarkedCompletedIsNotCompleted(){
+    public void ifNotMarkedAsCompletedIsNotCompleted(){
         management.add("New exercise");
         management.markAsCompleted("New exercise");
         assertFalse(management.isCompleted("Some exercise"));
     }
+     
 }
